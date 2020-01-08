07579a00625c2b3671829039fa5346c0122bef9b
anastayaa <anas.tayaagi@gmail.com>
2020-01-08 15:25:16 +0100

    removing controller annotation and adding RestController annotation

--- src/main/java/com/example/webappheroku/WebAppHerokuApplication.java
+++ src/main/java/com/example/webappheroku/WebAppHerokuApplication.java
@@ -2,10 +2,10 @@ package com.example.webappheroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @SpringBootApplication
 public class WebAppHerokuApplication {
 
